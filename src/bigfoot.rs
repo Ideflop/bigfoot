02ef37c8d7b2851b0c05aff672505b308d486ad3
Ideflop <vi03pl@gmail.com>
2023-09-19 17:57:59 +0100

    bigfoot.rs: remove some bug and improved the struct

--- src/bigfoot.rs
+++ src/bigfoot.rs
@@ -1,78 +1,95 @@
 const SIZE_VEC_RESERVED: usize = 1000;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BigFoot {
     is_signed: bool,
+    is_negative: bool,
     number: Vec<u8>,
 }
 
 impl BigFoot {
-    // TODO: should only accept digit and dot else error
+    // TODO: should only accept digit, and one dot else error
+    // Default -> Creation of Struct BigFoot with 0
     pub fn new<T>(is_signed: bool, value: T) -> BigFoot
-    where 
+    where
         T: Into<String>,
-    { 
+    {
         let string_value: String = value.into();
+        if string_value.is_empty() {
+            create_bigfoot(false, false, String::from("0"), SIZE_VEC_RESERVED);
+        }
         let size = if string_value.len() > SIZE_VEC_RESERVED {
             string_value.len().saturating_mul(10)
         } else {
             SIZE_VEC_RESERVED
         };
+        if is_signed {
+            from_i_to_big(string_value)
+        } else {
+            create_bigfoot(is_signed, false, string_value, size)
+        }
 
-        create_big(is_signed, string_value, size)
     }
 
-    pub fn from_unsigned<T: Into<u128>>(value: T) -> BigFoot { 
-        create_big(false, Into::<u128>::into(value).to_string() , SIZE_VEC_RESERVED)
+    pub fn from_unsigned<T: Into<u128>>(value: T) -> BigFoot {
+        create_bigfoot(
+            false,
+            false,
+            Into::<u128>::into(value).to_string(),
+            SIZE_VEC_RESERVED,
+        )
     }
 
-    pub fn from_usize(value: usize) -> BigFoot { 
-        create_big(false, value.to_string(), SIZE_VEC_RESERVED)
+    pub fn from_usize(value: usize) -> BigFoot {
+        create_bigfoot(false, false, value.to_string(), SIZE_VEC_RESERVED)
     }
 
-    pub fn from_signed<T: Into<i128>>(value: T) -> BigFoot { 
+    pub fn from_signed<T: Into<i128>>(value: T) -> BigFoot {
         from_i_to_big(Into::<i128>::into(value).to_string())
     }
 
-    pub fn from_isize(value: isize) -> BigFoot { 
+    pub fn from_isize(value: isize) -> BigFoot {
         from_i_to_big(value.to_string())
     }
 
-    // TODO: the size and the number are chosen randomly
-    // the user must give the size of the number
+    // TODO: the user must give the size of the number
+    // but the number are chosen randomly
     // ex : 3 => 000
     //      10 => 0000000000
     fn new_random() -> Self {
         todo!()
     }
-
 }
 
 fn from_i_to_big(mut value: String) -> BigFoot {
-    value.retain(|c| c != '-');
-    create_big(true, value, SIZE_VEC_RESERVED)
+    let mut is_negative = false;
+    if value.contains("-") {
+        is_negative = true;
+        value.retain(|c| c != '-');
+    }
+    create_bigfoot(true, is_negative, value, SIZE_VEC_RESERVED)
 }
 
-// do the actual creation of the BigFoot struct
-fn create_big(is_signed: bool, number: String, size: usize) -> BigFoot {
+// do the creation of the BigFoot struct
+fn create_bigfoot(is_signed: bool, is_negative: bool, number: String, size: usize) -> BigFoot {
     let mut number_as_vec = number.as_bytes().to_vec();
     number_as_vec.reserve(size);
 
-    BigFoot { 
-        is_signed, 
-        number: number_as_vec,  
-    } 
+    BigFoot {
+        is_signed,
+        is_negative,
+        number: number_as_vec,
+    }
 }
 
 //trait Addition {
 //    fn add(&self, other: BigFoot) -> BigFoot;
 //}
 
-
 impl std::fmt::Display for BigFoot {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        if self.is_signed {
-           write!(f,"-")?;
+        if self.is_signed && self.is_negative {
+            write!(f, "-")?;
         };
         for n in self.number.iter() {
             // TODO: check if checked_sub is the right function to do it
@@ -87,3 +104,6 @@ impl std::fmt::Display for BigFoot {
         Ok(())
     }
 }
+
+
+//TODO: implement to_string for struct BigFoot
